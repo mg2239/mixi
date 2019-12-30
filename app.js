dab88dcb5862cb9dd47a48fa5639bfc423c2f15f
Matthew Guo <mg2239@cornell.edu>
2019-12-29 23:56:44 -0500

    added authentication code from spotify demo code, started planning out pages

---
+++ app.js
@@ -0,0 +1,96 @@
+require('dotenv').config();
+const express = require('express');
+const request = require('request');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const querystring = require('querystring');
+
+const client_id = process.env.CLIENT_ID;
+const client_secret = process.env.CLIENT_SECRET;
+const redirect_uri = 'http://localhost:8888/callback';
+
+const stateKey = 'spotify_auth_state';
+
+const app = express();
+
+app.use(express.static(__dirname + '/public'))
+  .use(cors())
+  .use(cookieParser());
+
+/**
+ * Generates a random string containing numbers and letters
+ * @param  {number} length The length of the string
+ * @return {string} The generated string
+ */
+function generateRandomString(length) {
+  let text = '';
+  const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  for (let i = 0; i < length; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+};
+
+app.get('/login', function (req, res) {
+  let state = generateRandomString(16);
+  res.cookie(stateKey, state);
+
+  let scope = 'user-read-private user-read-email';
+  res.redirect('https://accounts.spotify.com/authorize?' +
+    querystring.stringify({
+      response_type: 'code',
+      client_id: client_id,
+      scope: scope,
+      redirect_uri: redirect_uri,
+      state: state
+    }));
+});
+
+app.get('/callback', function (req, res) {
+  let code = req.query.code || null;
+  let state = req.query.state || null;
+  let storedState = req.cookies ? req.cookies[stateKey] : null;
+  if (state === null || state !== storedState) {
+    res.redirect('/#' +
+      querystring.stringify({
+        error: 'state_mismatch'
+      }));
+  }
+  else {
+    res.clearCookie(stateKey);
+    let authOptions = {
+      url: 'https://accounts.spotify.com/api/token',
+      form: {
+        code: code,
+        redirect_uri: redirect_uri,
+        grant_type: 'authorization_code'
+      },
+      headers: {
+        'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
+      },
+      json: true
+    };
+    request.post(authOptions, function (error, response, body) {
+      if (!error && response.statusCode === 200) {
+        let { access_token, refresh_token } = body;
+        // let options = {
+        //   url: 'https://api.spotify.com/v1/me',
+        //   headers: { 'Authorization': 'Bearer ' + access_token },
+        //   json: true
+        // };
+        // request.get(options, function (error, response, body) {
+        //   console.log(body);
+        // });
+        res.redirect('playlist-select.html?' + querystring.stringify({
+          access_token, refresh_token
+        }));
+      }
+      else {
+        res.redirect('error.html')
+      }
+    });
+  }
+});
+
+console.log('Listening on 8888');
+app.listen(8888);
