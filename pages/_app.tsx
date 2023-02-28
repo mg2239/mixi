50b453b4394a224c3dc09602fdeadd1658bfbbd1
Matthew Guo <mguo22@gmail.com>
2023-02-28 00:17:21 -0500

    homepage

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
-import 'styles/globals.css';
 import type { AppProps } from 'next/app';
+import { Inter } from 'next/font/google';
+import 'styles/globals.css';
+
+const inter = Inter({
+  subsets: ['latin'],
+  variable: '--font-inter',
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <main className={`${inter.variable} font-sans`}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,38 @@
-import { Inter } from 'next/font/google';
-
-const inter = Inter({ subsets: ['latin'] });
-
-export default function Home() {
+const Home = () => {
   return (
-    <div>
-      <p>hello</p>
+    <div className="flex flex-col items-center py-16 text-center">
+      <div className="mb-5 w-36">
+        <img src="mixi-logo.png" alt="mixi logo" className="w-36" />
+        <a href="/" className=" no-underline">
+          <h1 className="text-5xl font-bold">Mixi</h1>
+        </a>
+      </div>
+      <p className="mb-4 text-xl">
+        Make mixes faster by sorting your Spotify playlist tracks by key and
+        BPM!
+      </p>
+      <a href="/" className="mb-4">
+        <button
+          className="bg-green-500 px-4 py-2 text-sm text-white"
+          type="button"
+        >
+          Access with Spotify
+        </button>
+      </a>
+      <p>
+        built by{' '}
+        <a
+          href="https://github.com/mg2239"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          matt
+        </a>{' '}
+        on github
+      </p>
+      {/* {token && <PlaylistInput accessToken={token} />} */}
     </div>
   );
-}
+};
+
+export default Home;
