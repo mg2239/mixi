ce5ca0943691569d3f544be0fa8d2a0ef1b7f4f2
Matthew Guo <mguo22@gmail.com>
2023-02-27 23:52:57 -0500

    init

--- src/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './css/index.css';
-import './css/normalize.css';
-import './css/skeleton.css';
-import Login from './components/Home';
-
-ReactDOM.render(<Login />, document.getElementById('root'));
